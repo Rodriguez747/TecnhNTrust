048ab7df3565cecc5853ee077ea5c02c83224fc6
Rodriguez747 <ahronchristian.rodriguez@cvsu.edu.ph>
2025-07-28 09:49:49 +0800

    Add files via upload

---
+++ TechNTrust 2-5/main.js
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Home() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/wH4V06EMIB7TJBEj/scene.splinecode" 
+      />
+    </main>
+  );
+}
